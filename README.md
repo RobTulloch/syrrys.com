bc4e1e62451775cbe0666e6dbf9fd3ee3c3f40c6
Rob Tulloch <31168192+RobTulloch@users.noreply.github.com>
2017-09-18 12:34:12 -0500

    Added all existing files from the host

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# syrrys.com
-Personal Website of Rob Tulloch.  
-
-The development plan is to use this to practice CSS Grid while providing links to the websites for my other projects.
+#Syrrys.com the whole reason for all these other domains
---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head profile="http://gmpg.org/xfn/11">
+<title>Syrrys.com</title>
+<?php include_once("analyticstracking.php"); ?>
+</head>
+<body>
+	
+	<h1>Syrrys.com</h1>
+	<p><del>I'm (still) in the middle of reorganizing, excuse the mess.</del>
+	That's become slightly redundant.
+	<p>Welcome to an index of a few of my projects. Hopefully you can find what you're looking for or at least something.
+	<p>
+	My resume is available at <a href="http://robtulloch.com">RobTulloch.com</a> <!-- Please hire me -->
+	<p> My .Plan file </p>
+<object data=".plan" 
+	type='application/txt'>
+<!--	iframe html (fall back case) -->
+	<iframe src=".plan" 
+			style="border: none;">
+<!-- embed html (last chance) -->
+			<embed src=".plan" type='application/txt'></embed> 
+	</iframe>
+</object>
+
+
+<ul>
+<li><a href="http://alice.syrrys.com/INDEX.php">Alice (circa 1997)</a></li>
+<li><a href="http://test.syrrys.com/">Testing?</a></li>
+<li><a href="http://intelligentdecline.com/">Intelligent Decline blog</a>
+<li><a href="http://blog.syrrys.com" rel="me">My Wordpress Blog (This could use some attention)</a>
+<li><a href="http://exceptionallyclassy.com/">Exceptionally Classy (Clothing... soon)</a>
+<li><a href="http://integral-designs.net/">Integral Designs (uses flash... aparently)</a>
+
+</ul>
+<foot><a href="http://gmpg.org/xfn">  <img src="xfn-btn.gif" alt="XFN Friendly" /> </a></foot>
+</body></html>
