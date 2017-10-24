1ce335a09aa048ee2cf96530b67a2a9bf5435849
Rob Tulloch <31168192+RobTulloch@users.noreply.github.com>
2017-10-24 18:44:08 -0500

    Moved affiliate link added online status

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<?php include_once("analyticstracking.php"); ?>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<meta charset="utf-8">
+		<META name="author" content="Rob Tulloch">
 		<title>Syrrys.com</title>
 		<style>
 			h1 {
@@ -32,7 +33,7 @@
 			.box {
 				background-color: #999;
 				columns: 1fr;
-				color: #fff;
+				color: #000;
 				border-radius: 5px;
 				padding: 20px;
 				font-size: 100%;
@@ -66,30 +67,50 @@
 			<!-- embed html (last chance) -->
 						<embed src=".plan.html" type='application/html'></embed>
 				</iframe>
-			</object>
+			</object><br/>
+			<code>and keep getting more organized. I've been using <a href="https://www.amazon.com/gp/product/B013Z5Y70K/ref=as_li_tl?ie=UTF8&tag=syrrys-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=B013Z5Y70K&linkId=38f8aab83e176bec5d455b9b014f26ff">Panda Planner</a> and <a href="https://todoist.com/">Todoist</a>.</code>
 		</div>
 
 		<div class="box list">
 			<h2>My Other Sites</h2>
 			<ul>
-			<li><a href="http://alice.syrrys.com/INDEX.php">Alice (circa 1997)</a></li>
+			<li><a href="http://alice.syrrys.com/INDEX.php">Alice (circa 1997): a "Rich-media" rendition Chapter 7 of Through the Looking Glass and what alice found there</a></li>
 			<li><a href="http://test.syrrys.com/">Testing?</a></li>
-			<li><a href="http://intelligentdecline.com/">Intelligent Decline blog</a>
+			<li><a href="http://intelligentdecline.com/" rel="me">Intelligent Decline blog</a>
 			<li><a href="http://blog.syrrys.com" rel="me">My Wordpress Blog (This could use some attention)</a>
-			<li><a href="http://exceptionallyclassy.com/">Exceptionally Classy (Denim &amp; Fragrance Sales)</a>
-			<li><a href="http://integral-designs.net/">Integral Designs - Smart Home Automation and consulting &amp; Family Online Security Planning/Auditing</a>
+			<li><a href="http://exceptionallyclassy.com/" rel="me">Exceptionally Classy (Denim &amp; Fragrance Sales)</a>
+			<li><a href="http://integral-designs.net/" rel="me">Integral Designs - Smart Home Automation and consulting &amp; Family Online Security Planning/Auditing</a>
 
 			</ul>
 		</div>
+
+	<div class="text blank">
+
 	</div>
 
-		<foot>
-			<a href="http://gmpg.org/xfn">  <img src="xfn-btn.gif" alt="XFN Friendly" /> </a>
+	<foot>
+
+<br/>
+
+			<a href="http://gmpg.org/xfn"><img src="xfn-btn.gif" alt="XFN Friendly"/></a>
 			<br>This page has been hand coded by me, based on various examples and tutorials
 			along with my cutting edge (1992-1997) personal experience. <br><small>I have dispensed
 			with the <code>blink</code> tags that used to be de riguer. You're welcome.</small>
 
 		</foot>
+		<br/>
+
+		</div>
 	</body>
-	<footer>This site uses cookies for Google Analytics tracking.</footer>
+	<footer>This site uses cookies for Google Analytics tracking.<br>
+		<object data="onlinetest.html"
+			type='application/html'>
+		<!--	iframe html (fall back case) -->
+			<iframe src="onlinetest.html"
+					style="border: none;">
+		<!-- embed html (last chance) -->
+					<embed src="onlinetest.html" type='application/html'></embed>
+			</iframe>
+		</object>
+	</footer>
 </html>
